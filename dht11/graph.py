fbc9c775293f099ef566ec9a179203b9cae3fdf9
m0r016 <x.vocaro39@gmail.com>
2021-08-30 00:21:25 +0900

    add contents

---
+++ dht11/graph.py
@@ -0,0 +1,32 @@
+import RPi.GPIO as GPIO
+import dht11
+import time
+import sys
+import csv
+import datatime
+import slackweb
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.cleanup()
+
+instance = dht11.DHT11(pin=14)
+
+result = instance.read()
+temp = result.temperature
+humidity = result.humidity
+
+while True:
+    try:
+        val = [0, 0, 0]
+        val[0] = datatime.datatime.now()
+        val[1] = temp
+        val[2] = humidity
+        print('now time ' + str(val[0]) + ',' + ' now temp ' +
+              str(val[1]) + ',' + 'now humidity' + str(val[2]))
+
+        with open('./log.csv', 'a') as f:
+            writer = csv.writer(f)
+            writer.writerow(val)
+    except (KeyboardInterrupt, SystemExit):
+        cleanAndExit()
